09d6138ccdbc539fec709797f6c8cb63a580d144
Tom Witkowski <dev.gummibeer@gmail.com>
2021-03-14 18:09:58 +0100

    install horizon

---
+++ app/Providers/HorizonServiceProvider.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Str;
+use Laravel\Horizon\Horizon;
+use Laravel\Horizon\HorizonApplicationServiceProvider;
+
+class HorizonServiceProvider extends HorizonApplicationServiceProvider
+{
+    public function boot(): void
+    {
+        parent::boot();
+
+
+        if (config('horizon.notifications.sms') !== null) {
+            Horizon::routeSmsNotificationsTo(config('horizon.notifications.sms'));
+        }
+
+        if (config('horizon.notifications.email') !== null) {
+            Horizon::routeMailNotificationsTo(config('horizon.notifications.email'));
+        }
+
+        if (
+            config('horizon.notifications.slack.webhook') !== null
+            && config('horizon.notifications.slack.channel') !== null
+        ) {
+            Horizon::routeSlackNotificationsTo(
+                config('horizon.notifications.slack.webhook'),
+                Str::start(config('horizon.notifications.slack.channel'), '#')
+            );
+        }
+
+        // Horizon::night();
+    }
+
+    protected function gate(): void
+    {
+        Gate::define('viewHorizon', static function (User $user): bool {
+            return in_array($user->email, [
+                //
+            ]);
+        });
+    }
+}
--- config/database.php
+++ config/database.php
@@ -130,16 +130,30 @@ return [
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_DB', '0'),
+            'port' => env('REDIS_PORT', 6379),
+            'database' => env('REDIS_DB', 0),
         ],
 
         'cache' => [
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_CACHE_DB', '1'),
+            'port' => env('REDIS_PORT', 6379),
+            'database' => env('REDIS_CACHE_DB', 1),
+        ],
+
+        'queue' => [
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port' => env('REDIS_PORT', 6379),
+            'database' => env('REDIS_QUEUE_DB', 2),
+        ],
+
+        'horizon' => [
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port' => env('REDIS_PORT', 6379),
+            'database' => env('REDIS_HORIZON_DB', 3),
         ],
 
     ],
