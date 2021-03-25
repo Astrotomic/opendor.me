019d344c068e33b764997381b3eafce72a69cd53
Tom Witkowski <dev.gummibeer@gmail.com>
2021-03-25 18:26:39 +0100

    remove add new repository form

--- routes/app.php
+++ routes/app.php
@@ -1,16 +1,10 @@
 <?php
 
-use App\Models\Repository;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Contracts\View\View as ViewContract;
 
-Route::get('contributions', function () {
+Route::get('contributions', static function (): ViewContract {
     return view('app.contributions', [
         'contributions' => auth()->user()->contributions->load('owner'),
     ]);
 })->name('contributions');
-
-Route::post('repository', function (Illuminate\Http\Request $request) {
-    Repository::fromName($request->input('name'));
-
-    return redirect()->back();
-})->name('repository.create');
