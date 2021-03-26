cdbdd6224f5834eeb430cbc0e918d09471091eff
Tom Witkowski <dev.gummibeer@gmail.com>
2021-03-26 15:26:00 +0100

    add unreleased block reason

--- app/Enums/BlockReason.php
+++ app/Enums/BlockReason.php
@@ -10,6 +10,7 @@ use Spatie\Enum\Laravel\Enum;
  * @method static self SPAM()
  * @method static self INAPPROPRIATE()
  * @method static self DUPLICATE()
+ * @method static self UNRELEASED()
  * @method static self REVIEW()
  */
 final class BlockReason extends Enum
