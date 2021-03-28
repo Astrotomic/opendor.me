68c1ed9a982ca196d904a72a7842bc26a5e9c75b
Tom Witkowski <dev.gummibeer@gmail.com>
2021-03-28 04:08:52 +0200

    fix unauthenticated requests

--- resources/views/components/layout/web.blade.php
+++ resources/views/components/layout/web.blade.php
@@ -108,10 +108,12 @@
                     <!-- Profile dropdown -->
                     <div x-data="{ open: false }" @keydown.escape.stop="open = false" @click.away="open = false" class="ml-4 relative flex-shrink-0">
                         <div>
+                            @if($auth)
                             <button type="button" class="bg-white rounded-full flex text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500" id="user-menu" @click="open = !open" aria-haspopup="true" x-bind:aria-expanded="open">
                                 <span class="sr-only">Open user menu</span>
                                 <x-gh-avatar :model="$auth" class="h-8 w-8"/>
                             </button>
+                            @endif
                         </div>
 
                         <div x-description="Dropdown menu, show/hide based on menu state." x-show="open" x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu" style="display: none;">
