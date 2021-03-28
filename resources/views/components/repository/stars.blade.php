245ca4eea5567b86402c3bab3fce4312fde2e59f
Tom Witkowski <dev.gummibeer@gmail.com>
2021-03-29 00:00:28 +0200

    hide stars if they are zero

--- resources/views/components/repository/stars.blade.php
+++ resources/views/components/repository/stars.blade.php
@@ -1,6 +1,8 @@
 <?php /** @var int $stars */ ?>
 
+@if($stars > 0)
 <span class="inline-flex items-center px-1 py-0.5 rounded text-xs font-medium">
     <x-fas-star class="w-3.5 h-3.5 mr-1 text-yellow-500"/>
     <span class="text-yellow-800">{{ $stars }}</span>
 </span>
+@endif
