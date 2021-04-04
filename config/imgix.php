b8a4c8f89e1fe9f57d83d1cfe2e5bde14508d04e
Tom Witkowski <dev.gummibeer@gmail.com>
2021-04-04 17:41:37 +0200

    fix home hero image and use imgix package for unsplash images

---
+++ config/imgix.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'default' => 'unsplash',
+
+    'sources' => [
+        'unsplash' => [
+            'domain' => 'images.unsplash.com',
+            'useHttps' => true,
+            'signKey' => null,
+            'includeLibraryParam' => false,
+        ],
+    ],
+];
--- resources/views/web/home.blade.php
+++ resources/views/web/home.blade.php
@@ -17,7 +17,7 @@
         <div class="overflow-x-hidden relative pt-6 pb-16 sm:pb-24 lg:pb-32">
             <main class="px-4 mx-auto mt-16 max-w-7xl sm:mt-24 sm:px-6 lg:mt-32">
                 <div class="lg:flex lg:justify-between">
-                    <div class="sm:text-center md:max-w-2xl md:mx-auto lg:mx-0 lg:text-left lg:pr-12">
+                    <div class="sm:text-center md:max-w-2xl md:mx-auto lg:mx-0 lg:text-left lg:mr-12 lg:flex-grow">
                         <h1 class="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl xl:text-6xl">
                           We <span class="text-indigo-600">open doors</span> for open-source contributors
                         </h1>
@@ -31,7 +31,7 @@
                             <x-web.home.user-autocomplete class="mt-3"/>
                         </div>
                     </div>
-                    <div class="relative mt-12 sm:max-w-lg sm:mx-auto lg:mt-0 lg:max-w-none lg:mx-0 lg:flex lg:items-center">
+                    <div class="relative mt-12 sm:max-w-lg sm:mx-auto lg:mt-0 xl:max-w-none lg:mx-0 lg:flex lg:flex-shrink lg:items-center">
                         <svg class="absolute top-0 left-1/2 transform origin-top scale-75 -translate-x-1/2 -translate-y-8 sm:scale-100 lg:hidden" width="640" height="784" fill="none" viewBox="0 0 640 784" aria-hidden="true">
                             <defs>
                                 <pattern id="4f4f415c-a0e9-44c2-9601-6ded5a34a13e" x="118" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
@@ -41,14 +41,15 @@
                             <rect y="72" width="640" height="640" class="text-gray-50" fill="currentColor" />
                             <rect x="118" width="404" height="784" fill="url(#4f4f415c-a0e9-44c2-9601-6ded5a34a13e)" />
                         </svg>
-                        <div class="overflow-hidden relative mx-auto w-full rounded-lg shadow-lg">
-                            <img
-                                class="w-full"
-                                src="https://images.unsplash.com/photo-1573497491208-6b1acb260507?auto=format&fit=crop&w=1350&q=80"
-                                alt="Two people in interview process"
-                                loading="lazy"
-                            />
-                        </div>
+                        <x-imgix
+                            source="unsplash"
+                            path="photo-1573497491208-6b1acb260507"
+                            class="relative z-10 w-full rounded-lg shadow-lg"
+                            alt="Two people during interview process"
+                            width="1350"
+                            height="900"
+                            :params="['fit' => 'crop', 'rot' => 357]"
+                        />
                     </div>
                 </div>
             </main>
