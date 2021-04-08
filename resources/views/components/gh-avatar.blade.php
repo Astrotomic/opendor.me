99e9a722b3a28cbadcc3f6d1bee74949fd499d95
Tom Witkowski <dev.gummibeer@gmail.com>
2021-04-08 16:50:06 +0200

    define avatar width and height

--- resources/views/components/gh-avatar.blade.php
+++ resources/views/components/gh-avatar.blade.php
@@ -6,5 +6,7 @@
     :src="$model->avatar_url"
     :alt="$model->name"
     loading="lazy"
+    width="192"
+    height="192"
     {{ $attributes->merge(['class' => 'flex-shrink-0 rounded-md bg-white']) }}
 />
--- resources/views/components/web/home/random-organizations.blade.php
+++ resources/views/components/web/home/random-organizations.blade.php
@@ -6,7 +6,7 @@
         <div class="mt-6 grid grid-cols-2 gap-0.5 md:grid-cols-3 lg:mt-8">
             @foreach($organizations() as $organization)
                 <div class="flex flex-col col-span-1 justify-center items-center py-8 px-8 space-y-2 bg-gray-50">
-                    <x-gh-avatar :model="$organization" class="max-h-14"/>
+                    <x-gh-avatar :model="$organization" class="h-14 w-14"/>
                     <span class="text-lg font-medium">{{ \Illuminate\Support\Str::title($organization->name) }}</span>
                 </div>
             @endforeach
