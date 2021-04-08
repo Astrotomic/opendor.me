5f1276b4a75a328867ab5677c48287e758c496d4
Tom Witkowski <dev.gummibeer@gmail.com>
2021-04-08 22:57:20 +0200

    backup before deployment

--- config/backup.php
+++ config/backup.php
@@ -233,7 +233,7 @@ return [
             /*
              * The number of days for which backups must be kept.
              */
-            'keep_all_backups_for_days' => 7,
+            'keep_all_backups_for_days' => 2,
 
             /*
              * The number of days for which daily backups must be kept.
