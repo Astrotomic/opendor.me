6fd29399035dc5320527417dd1d48ca6a72a6395
Tom Witkowski <dev.gummibeer@gmail.com>
2021-04-09 16:22:05 +0200

    unset unauthorized github tokens

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,19 +4,24 @@ namespace App\Providers;
 
 use App\Models\User;
 use Closure;
+use GuzzleHttp\Promise\PromiseInterface;
 use Illuminate\Contracts\Container\Container;
 use Illuminate\Contracts\Filesystem\Filesystem as FilesystemContract;
 use Illuminate\Filesystem\FilesystemAdapter;
 use Illuminate\Http\Client\PendingRequest;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
 use League\Flysystem\Filesystem;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
 use Spatie\Dropbox\Client as Dropbox;
 use Spatie\FlysystemDropbox\DropboxAdapter;
 use Stillat\Numeral\Languages\LanguageManager;
 use Stillat\Numeral\Numeral;
+use Symfony\Component\HttpFoundation\Response;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -50,6 +55,35 @@ class AppServiceProvider extends ServiceProvider
                 ->accept('application/vnd.github.v3+json')
                 ->withUserAgent(config('app.name').' '.config('app.url'))
                 ->withOptions(['http_errors' => true])
+                ->withMiddleware(function (callable $handler): Closure {
+                    return function (RequestInterface $request, array $options) use ($handler): PromiseInterface {
+                        $promise = $handler($request, $options);
+
+                        return $promise->then(function (ResponseInterface $response) use ($request): ResponseInterface {
+                            if ($response->getStatusCode() !== Response::HTTP_UNAUTHORIZED) {
+                                return $response;
+                            }
+
+                            $token = (string) Str::of(Arr::first($request->getHeader('Authorization')))
+                                ->after('Bearer')
+                                ->trim();
+
+                            if (empty($token)) {
+                                return $response;
+                            }
+
+                            $user = User::firstWhere('github_access_token', $token);
+
+                            if ($user === null) {
+                                return $response;
+                            }
+
+                            $user->forceFill(['github_access_token' => null])->save();
+
+                            return $response;
+                        });
+                    };
+                })
                 ->when(
                     User::whereIsRegistered()->inRandomOrder()->first()?->github_access_token,
                     fn (PendingRequest $request, $token) => $request->withToken($token)
