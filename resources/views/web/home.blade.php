29311af496ac3e04761cfbbc6d8bdd54a6c52442
Tom Witkowski <dev.gummibeer@gmail.com>
2021-04-09 23:41:49 +0200

    add home OG image

--- resources/views/web/home.blade.php
+++ resources/views/web/home.blade.php
@@ -1,5 +1,7 @@
 @push('head')
     <meta name="description" content="We open doors for open-source contributors and make open-source visible and understandable for everyone and show recruiters the open-source work of a potential candidate."/>
+
+    <x-open-graph.website :image="asset('images/og-home.png')"/>
 @endpush
 
 <x-layout.web>
