3c0d82f1cc5baf36b9f4f16de1b78eabe104af44
Tom Witkowski <dev.gummibeer@gmail.com>
2021-04-09 18:17:16 +0200

    fix plausible CSP

--- app/Http/Policies/ContentSecurityPolicy.php
+++ app/Http/Policies/ContentSecurityPolicy.php
@@ -17,6 +17,7 @@ class ContentSecurityPolicy extends Policy
             ->addDirective(Directive::DEFAULT, Keyword::SELF)
             ->addDirective(Directive::CHILD, Keyword::NONE)
             ->addDirective(Directive::CONNECT, Keyword::SELF)
+            ->addDirective(Directive::CONNECT, 'https://plausible.io/api/event')
             ->addDirective(Directive::FORM_ACTION, Keyword::SELF)
             ->addDirective(Directive::FRAME, Keyword::NONE)
             ->addDirective(Directive::FRAME_ANCESTORS, Keyword::NONE)
