10b0cd557b12ce146112a78c81a3d795b95f4fcc
Tom Witkowski <dev.gummibeer@gmail.com>
2021-04-10 22:37:44 +0200

    fix most used language

--- app/Http/Controllers/Web/ShowUserProfileController.php
+++ app/Http/Controllers/Web/ShowUserProfileController.php
@@ -21,7 +21,7 @@ class ShowUserProfileController
 
         $contributions = $user->contributions()->with('owner')->cursor();
         $organizations = $user->organizations->filter(fn (Organization $organization): bool => $organization->repositories()->exists());
-        $languages = $contributions->pluck('language')->reject(Language::NOASSERTION())->unique()->collect()->values();
+        $languages = $contributions->pluck('language')->reject(Language::NOASSERTION())->collect()->values();
         $contributions = $contributions->groupBy('vendor_name')->sortBy(fn (Collection $repositories, string $owner): string => Str::lower($owner));
 
         return view('web.profile.user', [
