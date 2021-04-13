e2d9636f985cfbb1891b7a64a90728b2f840c9b4
Tom Witkowski <dev.gummibeer@gmail.com>
2021-04-13 23:17:49 +0200

    add ping route

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,24 @@
 <?php
 
 use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Arr;
 
-Route::get('user/autocomplete', static function () {
+Route::get('user/autocomplete', static function (): JsonResponse {
     return response()->json(
         User::whereIsRegistered()->get()->map(fn (User $user): array => $user->only('id', 'name', 'display_name'))
     );
 })->name('user.autocomplete');
+
+Route::get('ping', static function (Request $request): JsonResponse {
+    return response()->json([
+        'ping' => 'pong',
+        'env' => config('app.env'),
+        'headers' => array_map(
+            fn (array $headers) => count($headers) === 1 ? Arr::first($headers) : $headers,
+            $request->headers->all()
+        ),
+    ]);
+})->name('ping');
