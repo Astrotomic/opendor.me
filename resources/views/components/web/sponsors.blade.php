aa8e1abb267e1a03f58b018e4d90b42eb9f4f656
Tom Witkowski <dev.gummibeer@gmail.com>
2021-04-15 20:26:37 +0200

    add Enlightn sponsor

--- resources/views/components/web/sponsors.blade.php
+++ resources/views/components/web/sponsors.blade.php
@@ -4,11 +4,17 @@
             Sponsors
         </h2>
         <div class="grid grid-cols-2 gap-8 justify-center py-4 mt-6 md:grid-cols-3 lg:grid-cols-4">
-            <a href="https://pingping.io" class="flex justify-center py-6 px-4">
+            <a href="https://pingping.io" class="flex flex-col justify-center items-center py-6 px-4 space-y-2">
                 <img class="max-h-12" src="{{ asset('images/sponsors/pingping.svg') }}" alt="PingPing">
+                <span class="text-lg font-medium text-center">PingPing</span>
             </a>
-            <a href="https://plausible.io" class="flex justify-center py-6 px-4">
+            <a href="https://plausible.io" class="flex flex-col justify-center items-center py-6 px-4 space-y-2">
                 <img class="max-h-12" src="{{ asset('images/sponsors/plausible.png') }}" alt="Plausible">
+                <span class="text-lg font-medium text-center">Plausible</span>
+            </a>
+            <a href="https://www.laravel-enlightn.com" class="flex flex-col justify-center items-center py-6 px-4 space-y-2">
+                <img class="max-h-12" src="{{ asset('images/sponsors/enlightn.png') }}" alt="Enlightn">
+                <span class="text-lg font-medium text-center">Enlightn</span>
             </a>
         </div>
     </div>
