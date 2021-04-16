aa5b536bb927573ddb58927eb524d0ac095fe30d
Tom Witkowski <dev.gummibeer@gmail.com>
2021-04-16 10:36:39 +0200

    remove user observer listener

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -2,20 +2,6 @@
 
 namespace App\Observers;
 
-use App\Models\User;
-
 class UserObserver
 {
-    public function saving(User $user): void
-    {
-        if (empty($user->github_access_token)) {
-            $user->email = "{$user->id}+{$user->name}@users.noreply.github.com";
-            $user->email_verified_at = null;
-            $user->full_name = null;
-            $user->description = null;
-            $user->twitter = null;
-            $user->website = null;
-            $user->location = null;
-        }
-    }
 }
