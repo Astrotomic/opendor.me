7d2e24a2f09b55f469b6bcf9466997ce64a01ca7
Tom Witkowski <dev.gummibeer@gmail.com>
2021-04-18 14:44:24 +0200

    add missing languages

--- app/Enums/Language.php
+++ app/Enums/Language.php
@@ -41,6 +41,40 @@ use Spatie\Enum\Laravel\Enum;
  * @method static self HCL()
  * @method static self C()
  * @method static self PUPPET()
+ * @method static self ACTIONSCRIPT()
+ * @method static self ARDUINO()
+ * @method static self ASSEMBLY()
+ * @method static self CLOJURE()
+ * @method static self COLDFUSION()
+ * @method static self DART()
+ * @method static self EAGLE()
+ * @method static self ELM()
+ * @method static self GLSL()
+ * @method static self GNUPLOT()
+ * @method static self GROOVY()
+ * @method static self HACK()
+ * @method static self HAML()
+ * @method static self HANDLEBARS()
+ * @method static self JULIA()
+ * @method static self LESS()
+ * @method static self MAKEFILE()
+ * @method static self MATHEMATICA()
+ * @method static self MUSTACHE()
+ * @method static self NGINX()
+ * @method static self OCAML()
+ * @method static self OZ()
+ * @method static self PLPGSQL()
+ * @method static self POSTSCRIPT()
+ * @method static self RESCRIPT()
+ * @method static self RUST()
+ * @method static self SASS()
+ * @method static self SCALA()
+ * @method static self SQLPL()
+ * @method static self TEX()
+ * @method static self TSQL()
+ * @method static self TWIG()
+ * @method static self VHDL()
+ * @method static self WEBASSEMBLY()
  * @method static self NOASSERTION()
  */
 final class Language extends Enum
@@ -91,7 +125,40 @@ final class Language extends Enum
             'VIM_L' => 'VimL',
             'VISUAL_BASIC' => 'Visual Basic',
             'PUPPET' => 'Puppet',
+            'ACTIONSCRIPT' => 'ActionScript',
+            'ARDUINO' => 'Arduino',
             'ASSEMBLY' => 'Assembly',
+            'CLOJURE' => 'Clojure',
+            'COLDFUSION' => 'ColdFusion',
+            'DART' => 'Dart',
+            'EAGLE' => 'Eagle',
+            'ELM' => 'Elm',
+            'GLSL' => 'GLSL',
+            'GNUPLOT' => 'Gnuplot',
+            'GROOVY' => 'Groovy',
+            'HACK' => 'Hack',
+            'HAML' => 'Haml',
+            'HANDLEBARS' => 'Handlebars',
+            'JULIA' => 'Julia',
+            'LESS' => 'Less',
+            'MAKEFILE' => 'Makefile',
+            'MATHEMATICA' => 'Mathematica',
+            'MUSTACHE' => 'Mustache',
+            'NGINX' => 'Nginx',
+            'OCAML' => 'OCaml',
+            'OZ' => 'Oz',
+            'PLPGSQL' => 'PLpgSQL',
+            'POSTSCRIPT' => 'PostScript',
+            'RESCRIPT' => 'ReScript',
+            'RUST' => 'Rust',
+            'SASS' => 'Sass',
+            'SCALA' => 'Scala',
+            'SQLPL' => 'SQLPL',
+            'TEX' => 'TeX',
+            'TSQL' => 'TSQL',
+            'TWIG' => 'Twig',
+            'VHDL' => 'VHDL',
+            'WEBASSEMBLY' => 'WebAssembly',
             'NOASSERTION' => 'Other',
         ];
     }
@@ -103,6 +170,14 @@ final class Language extends Enum
             static::SMARTY()->value => 'gray-300',
             static::XML()->value => 'gray-300',
             static::HCL()->value => 'gray-300',
+            static::ARDUINO()->value => 'gray-300',
+            static::EAGLE()->value => 'gray-300',
+            static::GLSL()->value => 'gray-300',
+            static::MATHEMATICA()->value => 'gray-300',
+            static::NGINX()->value => 'gray-300',
+            static::PLPGSQL()->value => 'gray-300',
+            static::SQLPL()->value => 'gray-300',
+            static::TSQL()->value => 'gray-300',
             static::VIM_L()->value => static::VIM_SCRIPT()->value,
             static::VISUAL_BASIC()->value => 'visual-basic-net',
             default => $this->value,
