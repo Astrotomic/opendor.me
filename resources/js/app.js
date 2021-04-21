0859b83a02a1ceb0401b53987e9f19c9dd77cd3e
Dan Pock <self@danpock.me>
2021-04-21 11:46:33 -0400

    [ServiceWorker] Skip the homepage only when the special auth hash is set

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,3 +3,9 @@ import './alpine/fuse';
 import 'alpinejs';
 
 window.components = {};
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    if ('#newAuth' === location.hash) {
+        history.pushState("", document.title, window.location.pathname + window.location.search);
+    }
+});
