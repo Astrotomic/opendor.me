aa34b69bf8b18ef3feccbadeac8415c0d5430f9f
Tom Witkowski <dev.gummibeer@gmail.com>
2021-04-29 18:42:19 +0200

    spread LoadRepositoryContributors jobs

--- app/Console/Commands/GithubRepositoryContributors.php
+++ app/Console/Commands/GithubRepositoryContributors.php
@@ -6,8 +6,11 @@ use App\Jobs\LoadRepositoryContributors;
 use App\Models\Organization;
 use App\Models\Repository;
 use App\Models\User;
+use Carbon\CarbonInterval;
 use Illuminate\Console\Command;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Bus;
 
 class GithubRepositoryContributors extends Command
 {
@@ -29,8 +32,15 @@ class GithubRepositoryContributors extends Command
             ->reject(function (Repository $repository): bool {
                 return $repository->owner instanceof Organization && $repository->owner->members()->whereIsRegistered()->doesntExist();
             })
-            ->each(static function (Repository $repository): void {
-                LoadRepositoryContributors::dispatchBatch($repository);
+            ->groupBy('vendor_name')
+            ->each(static function (Collection $repositories): void {
+                $repositories
+                    ->chunk(100)
+                    ->each(static function (Collection $repositories, int $i) {
+                        $repositories->each(fn (Repository $repository) => Bus::batch([
+                            (new LoadRepositoryContributors($repository))->delay(CarbonInterval::minutes($i * 5)),
+                        ])->onQueue('github')->dispatch());
+                    });
             });
     }
 }
