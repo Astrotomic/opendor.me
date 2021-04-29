c46a8b8ca78bb950f60010c6ca7f7da792dff643
Tom Witkowski <dev.gummibeer@gmail.com>
2021-04-29 10:24:52 +0200

    purge all schedule logs older than 2 days - every task is run at least daily

--- config/schedule-monitor.php
+++ config/schedule-monitor.php
@@ -6,7 +6,7 @@ return [
      * After a while the `monitored_scheduled_task_log_items` might become big.
      * Here you can specify the amount of days log items should be kept.
      */
-    'delete_log_items_older_than_days' => 30,
+    'delete_log_items_older_than_days' => 2,
 
     /*
      * The date format used for all dates displayed on the output of commands
