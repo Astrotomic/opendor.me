df6b9123adf1649bbf07c20dc2ca851f71a00132
Tom Witkowski <dev.gummibeer@gmail.com>
2021-05-26 18:19:30 +0200

    fix tlint

--- app/Enums/Language.php
+++ app/Enums/Language.php
@@ -471,10 +471,12 @@ final class Language extends Enum
     protected static function values(): array
     {
         return once(fn () => self::languages()
-            ->mapWithKeys(fn (array $language): array => [$language['enum'] => match($language['name']) {
-                'Other' => 'OTHER',
-                default => $language['name'],
-            }])
+            ->mapWithKeys(fn (array $language): array => [
+                $language['enum'] => match ($language['name']) {
+                    'Other' => 'OTHER',
+                    default => $language['name'],
+                },
+            ])
             ->all());
     }
 
