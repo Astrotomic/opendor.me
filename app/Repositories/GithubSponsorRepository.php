ec4250b9b87f3d94142050bd2ca1e58a8b40f427
Tom Witkowski <tom.witkowski@frischepost.de>
2021-08-20 18:42:01 +0200

    replace custom GraphQL GitHub Sponsors by astrotomic package

--- app/Repositories/GithubSponsorRepository.php
+++ app/Repositories/GithubSponsorRepository.php
@@ -4,10 +4,12 @@ namespace App\Repositories;
 
 use App\Models\Organization;
 use App\Models\User;
+use Astrotomic\GithubSponsors\Facades\GithubSponsors;
 use Carbon\CarbonInterval;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Fluent;
 use Illuminate\Support\Str;
 
 class GithubSponsorRepository extends Repository
@@ -17,19 +19,19 @@ class GithubSponsorRepository extends Repository
         return Cache::remember(
             'github:sponsors',
             CarbonInterval::hour(),
-            fn (): Collection => Http::github()
-                ->withToken(config('services.github.sponsors_access_token'))
-                ->post('/graphql', ['query' => $this->query()])
-                ->collect('data.viewer.sponsorshipsAsMaintainer.nodes.*.sponsorEntity')
-                ->map(static function (array $sponsor): User | Organization | null {
+            fn (): Collection => collect()
+                ->merge(GithubSponsors::fromUser('Gummibeer')->select('databaseId', 'login')->all())
+                ->merge(GithubSponsors::fromUser('SarahSibert')->select('databaseId', 'login')->all())
+                ->unique('databaseId')
+                ->map(static function (Fluent $sponsor): User | Organization | null {
                     $sponsor['id'] = $sponsor['databaseId'];
 
                     if ($sponsor['__typename'] === 'User') {
-                        return User::fromGithub($sponsor);
+                        return User::fromGithub($sponsor->getAttributes());
                     }
 
                     if ($sponsor['__typename'] === 'Organization') {
-                        return Organization::fromGithub($sponsor);
+                        return Organization::fromGithub($sponsor->getAttributes());
                     }
 
                     return null;
@@ -38,30 +40,5 @@ class GithubSponsorRepository extends Repository
                 ->sortBy(fn (User | Organization $sponsor): string => Str::lower($sponsor->name))
                 ->values()
         );
-    }
-
-    protected function query(): string
-    {
-        return <<<'GRAPHQL'
-        {
-            viewer {
-                sponsorshipsAsMaintainer(first: 100) {
-                    nodes {
-                        sponsorEntity {
-                            __typename
-                            ... on User {
-                                databaseId
-                                login
-                            }
-                            ... on Organization {
-                                databaseId
-                                login
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        GRAPHQL;
     }
 }
