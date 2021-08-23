dbd25499f3e4256829615afdeb795db257a01402
Tom Witkowski <tom.witkowski@frischepost.de>
2021-08-23 18:54:59 +0200

    increase the timeout a bit to allow more active users to complete

--- app/Jobs/SyncUserContributions.php
+++ app/Jobs/SyncUserContributions.php
@@ -5,12 +5,14 @@ namespace App\Jobs;
 use App\Models\Repository;
 use App\Models\User;
 use Carbon\CarbonImmutable;
+use Carbon\CarbonInterval;
 
 class SyncUserContributions extends GithubJob
 {
     public function __construct(protected User $user)
     {
         parent::__construct();
+        $this->timeout = CarbonInterval::minutes(5)->totalSeconds;
     }
 
     public function run(): void
