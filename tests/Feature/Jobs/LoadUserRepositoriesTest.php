1fd918924c5dcb54ef8a86cce69988088e782766
Tom Witkowski <tom.witkowski@frischepost.de>
2021-09-27 12:52:48 +0200

    fix php cs

--- tests/Feature/Jobs/LoadUserRepositoriesTest.php
+++ tests/Feature/Jobs/LoadUserRepositoriesTest.php
@@ -5,11 +5,11 @@ use App\Models\User;
 use Carbon\CarbonInterval;
 
 it('has the expected configuration')
-    ->expect(fn() => new LoadUserRepositories(new User()))
+    ->expect(fn () => new LoadUserRepositories(new User()))
     ->timeout->toEqual(CarbonInterval::minutes(15)->totalSeconds)
     ->queue->toEqual('github');
 
-it('saves the users repositories', function() {
+it('saves the users repositories', function () {
     $this->user = $this->user('Gummibeer');
 
     LoadUserRepositories::dispatchSync($this->user);
@@ -17,9 +17,9 @@ it('saves the users repositories', function() {
     expect($this->user->repositories()->pluck('name')->toArray())
         ->toHaveCount(4)
         ->toEqualCanonicalizing([
-            "Gummibeer/alerts",
-            "Gummibeer/arma-rcon-class-php",
-            "Gummibeer/aire",
-            "Gummibeer/dotfiles",
+            'Gummibeer/alerts',
+            'Gummibeer/arma-rcon-class-php',
+            'Gummibeer/aire',
+            'Gummibeer/dotfiles',
         ]);
 });
