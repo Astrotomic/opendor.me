871a6b820b6d100c16ece6961a630601492fa2cd
Shift <shift@laravelshift.com>
2022-05-05 15:14:04 +0000

    Remove unnecessary `$model` property

--- database/factories/RepositoryFactory.php
+++ database/factories/RepositoryFactory.php
@@ -10,8 +10,6 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 
 class RepositoryFactory extends Factory
 {
-    protected $model = Repository::class;
-
     /**
      * Define the model's default state.
      *
