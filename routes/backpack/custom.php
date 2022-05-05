c6aa7188fcd7adcfb1c99f6bfb0bcb433326fb18
Tom Witkowski <tom.witkowski@frischepost.de>
2022-05-05 20:14:48 +0200

    fix route registration

--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -4,12 +4,7 @@ use App\Http\Controllers\Admin\OrganizationCrudController;
 use App\Http\Controllers\Admin\RepositoryCrudController;
 use App\Http\Controllers\Admin\UserCrudController;
 
-Route::prefix(config('backpack.base.route_prefix', 'admin'))->middleware(array_merge((array) config('backpack.base.web_middleware', 'web'), (array) config('backpack.base.middleware_key', 'admin')))->group([
-    'middleware' => array_merge(
-        (array) config('backpack.base.web_middleware', 'web'),
-        (array) config('backpack.base.middleware_key', 'admin')
-    ),
-], function () {
+Route::prefix(config('backpack.base.route_prefix', 'admin'))->middleware(array_merge((array) config('backpack.base.web_middleware', 'web'), (array) config('backpack.base.middleware_key', 'admin')))->group(static function () {
     Route::crud('user', UserCrudController::class);
     Route::crud('organization', OrganizationCrudController::class);
     Route::crud('repository', RepositoryCrudController::class);
